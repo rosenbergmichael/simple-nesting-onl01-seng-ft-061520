95829584cb30a56a95b0be06aed554f061c4b298
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-20 02:20:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -111,7 +111,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash[:alan_kay][:known_for] = "GUI"
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      #epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
      alans_new_info = "GUI"
