8713ab9aa6613daec0d39e42cf8daaaf3d9a239d
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-20 02:24:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -111,14 +111,14 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info = "GUI"
      puts programmer_hash
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     
-     alans_new_info = "GUI"
-     
-     
+  
 end
+
+
+
 
 def adding_to_dennis
 	programmer_hash = 
